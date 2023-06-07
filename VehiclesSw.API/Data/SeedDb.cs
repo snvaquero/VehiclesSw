f25fdd0df18e635b60e1ccd09d9ff7f3a84384f6
Erickson Antonio Vaquero Munguia <vaqueromunguia@gmail.com>
2023-06-07 10:11:41 -0600

    View PartialProcedure

---
+++ VehiclesSw.API/Data/SeedDb.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VehiclesSw.API.Data.Entities;
+
+namespace VehiclesSw.API.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            await CheckVehiclesTypesAsync();
+        }
+    
+        private async Task CheckVehiclesTypesAsync()
+        {
+            if (!_context.vehicleTypes.Any())
+            {
+                _context.vehicleTypes.Add(new VehicleType { Description = "Carro" });
+                _context.vehicleTypes.Add(new VehicleType { Description="Moto"});
+            }
+        }
+    }
+
+    }
+
